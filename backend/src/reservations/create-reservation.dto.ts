8d1e93b913cf13b1476d24860999cc5e9037bd6a
Hibiki Ono <173211997+SuperGorillas@users.noreply.github.com>
2026-03-08 22:33:52 +0900

    Implement reservation creation API(POST) 予約作成API(POST)を作成

---
+++ backend/src/reservations/create-reservation.dto.ts
@@ -0,0 +1,8 @@
+//POST（予約）で送られてくる、データ型の定義
+
+export class CreateReservationDto {
+    room_id: number ;
+    guest_count : number ;
+    check_in : string ;
+    check_out : string ;
+}
--- backend/src/reservations/reservations.service.ts
+++ backend/src/reservations/reservations.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { CreateReservationDto } from './create-reservation.dto';
 
 @Injectable()
-export class ReservationsService {}
+export class ReservationsService {
+    createReservation(createReservationDto: CreateReservationDto) {
+        return{//テストデータ
+            id: 1,
+            user_id: 1,
+            room_id: createReservationDto.room_id,
+            guest_count: createReservationDto.guest_count,
+            check_in: createReservationDto.check_in,
+            check_out: createReservationDto.check_out,
+            total_price: 24000,
+            status: 'CONFIREMED'
+        };
+    }
+}
