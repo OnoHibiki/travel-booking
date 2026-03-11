f6717942c3aff02df023679bd2f7d705e43055a0
Hibiki Ono <173211997+SuperGorillas@users.noreply.github.com>
2026-03-11 22:56:03 +0900

    Implement auth register and login APIs
    ユーザー登録APIとログインAPIを実装

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from './create-user.dto';
+import { LoginDto } from './login.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -9,5 +10,10 @@ export class AuthController {
     @Post('register')
     register(@Body() createUserDto: CreateUserDto) {
         return this.authService.register(createUserDto);
+    }
+
+    @Post('login')
+    login(@Body() { email, password }: LoginDto) {
+        return this.authService.login(email, password);
     }
 }
