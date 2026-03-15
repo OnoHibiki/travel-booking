c4b9359704392109cb64f098ca16144c0ea9476a
Hibiki Ono <173211997+SuperGorillas@users.noreply.github.com>
2026-03-15 16:05:43 +0900

    Implements user update API ユーザ情報更新APIを追加

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,4 +1,5 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, NotFoundException, BadRequestException, ConflictException } from '@nestjs/common';
+import { UpdateUserDto } from './update-user.dto';
 
 @Injectable()
 export class UsersService {
@@ -11,6 +12,7 @@ export class UsersService {
         },
     ];
 
+    // Get by self info - 自分の情報を取得
     findMe(userId: number) {
         const user = this.users.find((user) => user.id === userId);// only user Id - 今はIDだけ使う
 
@@ -20,5 +22,44 @@ export class UsersService {
 
         return user;
     }
+    
+    // Change by my info - 自分の情報を変更
+    updateMe(userId: number, updateUserDto: UpdateUserDto) {
+        const user = this.users.find((user) => user.id === userId);
+
+        if(!user) {
+            throw new NotFoundException('User not found. (ユーザが見つかりません)');
+        }
+        
+        if(updateUserDto.email) {
+            const existingUser = this.users.find((u) => u.email === updateUserDto.email && u.id !== userId);
+
+            if(existingUser) {
+                throw new ConflictException('This email already registered.(このメールアドレスはすでに登録されています)');
+            }
+        }
+
+        if(
+            updateUserDto.name === undefined &&
+            updateUserDto.email === undefined &&
+            updateUserDto.prefecture === undefined
+        ) {
+            throw new BadRequestException('No update fields provided.(更新項目が指定されていません)');
+        }
+
+        if(updateUserDto.name !== undefined) {
+            user.name = updateUserDto.name;
+        }
+        
+        if(updateUserDto.email !== undefined) {
+            user.email = updateUserDto.email;
+        }
+
+        if(updateUserDto.prefecture !== undefined) {
+            user.prefecture = updateUserDto.prefecture;
+        }
+    }
+
+
 
 }
